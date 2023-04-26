47b1e7dccb7bfae5358cfe407a9096827b520019
pratikk2609 <pratikpawar2609@gmail.com>
2023-04-26 18:26:16 +0530

    c

---
+++ programs/swap.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void swap(int *, int *);
+
+int main()
+{
+    int n1, n2;
+    printf("enter first no: ");
+    scanf("%d", &n1);
+    printf("enter seconf no: ");
+    scanf("%d", &n2);
+    
+    printf("before swapping n1:%d and n2:%d", n1, n2);
+    swap(&n1, &n2);
+    
+    printf("\nafter swapping n1:%d and n2:%d", n1, n2);
+}
+
+void swap(int *p, int *q)
+{
+    int temp;
+    temp=*p;
+    *p=*q;
+    *q=temp;
+}
