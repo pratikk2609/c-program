9e9152e7a24aafb0bac122371ebd002c85bd0ffc
pratikk2609 <pratikpawar2609@gmail.com>
2023-04-27 08:44:40 +0530

    c

--- programs/swap.c
+++ programs/swap.c
@@ -7,7 +7,7 @@ int main()
     int n1, n2;
     printf("enter first no: ");
     scanf("%d", &n1);
-    printf("enter seconf no: ");
+    printf("enter second no: ");
     scanf("%d", &n2);
     
     printf("before swapping n1:%d and n2:%d", n1, n2);
