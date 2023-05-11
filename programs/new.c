ad51fdeb1a0fc50f3e5fdce2b7b468588f554526
pratikk2609 <pratikpawar2609@gmail.com>
2023-05-11 20:28:04 +0530

    1st commit

--- programs/new.c
+++ programs/new.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    printf("hello world");
+    int a, b, c;
+    printf("Enter sides of triangle: ");
+    scanf("%d%d%d", &a, &b, &c);
+    if (a == b && b == c)
+        printf("Triangle is equilateral");
+    else if ((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (c * c) + (b * b) == (a * a))
+        printf("Triangle is Right Angled Triangle");
+    else if (a == b || b == c || c == a)
+        printf("Triangle is isosceles");
+    else
+        printf("Triangle is scalene");
+    return 0;
 }
