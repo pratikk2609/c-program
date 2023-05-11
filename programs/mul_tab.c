0bbe349eb29fd30cc26fe763ea8720bd9c1f0730
pratikk2609 <pratikpawar2609@gmail.com>
2023-05-11 20:46:44 +0530

    c

---
+++ programs/mul_tab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter a no: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    for (i=1;i<=10;i++)
+    {
+        printf("%d * %d = %d\n", n, i, n*i);
+    }
+}
