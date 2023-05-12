1938f353adc609d25dd92ad5d24f4be2288aa304
pratikk2609 <pratikpawar2609@gmail.com>
2023-05-12 20:32:10 +0530

    c

---
+++ programs/pattern1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x = 0, y = 0;
+    int squareSide = 0;
+    printf("Enter The Value ");
+    scanf("%u", &squareSide);
+
+    for (x = 0; x < squareSide; ++x)
+    {
+        for (y = 0; y < squareSide; ++y)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
