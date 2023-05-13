4d2b87370bb7114a3ddc7dcc70cdace0d39bb246
pratikk2609 <pratikpawar2609@gmail.com>
2023-05-13 21:16:26 +0530

    c

---
+++ programs/new3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, sv, ev, sum=0, temp;
+    printf("enter starting value:");
+    scanf("%d", &sv);
+    printf("enter ending value:");
+    scanf("%d", &ev);
+    if(sv > ev)
+
+    {
+        temp=sv;
+        sv=ev;
+        ev=temp;
+    }
+
+
+    for(i=sv; i<=ev; i++)
+    {
+        if(i % 4== 0){
+            sum=sum +i;
+        }
+ 
+       }
+       printf("sum is: %d" , sum);
+}
