fecddc8619a1510bf934083f79790333a24d4d02
pratikk2609 <pratikpawar2609@gmail.com>
2023-05-16 21:08:27 +0530

    c

---
+++ programs/new7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x, y = 0, r;
+    printf("enter a number: ");
+    scanf("%d", &x);
+
+    while (x != 0)
+    {
+        r = x % 10;
+        y = y * 10 + r;
+        x = x / 10;
+    }
+
+    printf("reverse is %d", y);
+    return 0;
+}
